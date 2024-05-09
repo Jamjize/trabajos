2c0640084c9e8f229fb2829caeb60cb64426daf0
Leo Pucheta <jamjizelol@gmail.com>
2024-05-09 04:56:05 -0300

    agregado boton limpiar para limpiar entry, funciona

--- administrador_negocio/GUI/inventory_tab.py
+++ administrador_negocio/GUI/inventory_tab.py
@@ -1,17 +1,5 @@
 import customtkinter
-from CTkTable import *
-
-class MyTable(CTkTable):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-
-        self.table = CTkTable(
-            master=self, 
-            row=kwargs.get("row"), 
-            column=kwargs.get("column"), 
-            values=kwargs.get("values")
-        )
-
+from .my_table import MyTable
 
 class FrameTabInventory(customtkinter.CTkFrame):
     def __init__(self, master):
@@ -33,15 +21,15 @@ class FrameTabInventory(customtkinter.CTkFrame):
         self.tb_name = customtkinter.CTkEntry(self, placeholder_text="Ej Tomate")
         self.tb_quantity = customtkinter.CTkEntry(self, placeholder_text="Ej 100")
         
-        self.btn_add = customtkinter.CTkButton(self, text="Ingresar", command=self.get_data)
-        self.btn_clear = customtkinter.CTkButton(self, text="Limpiar")
+        self.btn_add = customtkinter.CTkButton(self, text="Ingresar")
+        self.btn_clear = customtkinter.CTkButton(self, text="Limpiar", command=self.clear_enrty)
 
 
         #Colocando los widgets
         self.lbl_description.grid(row=0, column=0, columnspan=3, sticky="ew")
         self.lbl_name.grid(row=1, column=0, padx=10)
         self.lbl_quantity.grid(row=1, column=1, padx=10)
-        self.lbl_g.grid(row=1, column=2, padx=(0, 5), sticky="w")
+        self.lbl_g.grid(row=2, column=2, padx=(0, 5), sticky="w")
 
         self.tb_name.grid(row=2, column=0, padx=10)
         self.tb_quantity.grid(row=2, column=1, padx=(10,0))
@@ -50,18 +38,13 @@ class FrameTabInventory(customtkinter.CTkFrame):
         self.btn_clear.grid(row=3, column=1, padx=(10, 0), pady=20)
 
         #Tabla
-        values, amout_row = self.get_data()
-        self.table = MyTable(master=self, row=amout_row, column=2, values=values)
+        self.table = MyTable(master=self, row=1, column=2, values=[["Leo", "Pucheta"]])
         self.table.grid(row=4, column=0, columnspan=3, padx=20, pady=20, sticky="nesw")
 
-    def get_data(self):
-        #product_name = self.tb_name.get()
-        #product_quantity = self.tb_quantity.get()
-        values = [["Nombre", "apellido"],
-                  ["Leo", "Pucheta"],
-                  ["Maxi", "Acuña"],
-                  ["Pedro", "Picapiedra"],
-                  ["Jugan", "Gomes"],
-                  ["San", "Nicolas"],
-                  ["Nicola", "Tezla"],]
-        return values, 7
+    def tbn_add_item():
+        pass
+
+    def clear_enrty(self):
+        self.tb_name.delete(0, "end")
+        self.tb_quantity.delete(0, "end")
+
