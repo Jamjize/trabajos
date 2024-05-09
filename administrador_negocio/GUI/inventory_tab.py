898f9df29e188710710dc775b748356c841157cf
Leo Pucheta <jamjizelol@gmail.com>
2024-05-09 04:14:49 -0300

    Imprimir muchos nombres y apellidos en una tabla

--- administrador_negocio/GUI/inventory_tab.py
+++ administrador_negocio/GUI/inventory_tab.py
@@ -1,13 +1,21 @@
 import customtkinter
 from CTkTable import *
 
-table_values = [["Leo", "Maxi"]]
+class MyTable(CTkTable):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.table = CTkTable(
+            master=self, 
+            row=kwargs.get("row"), 
+            column=kwargs.get("column"), 
+            values=kwargs.get("values")
+        )
+
 
 class FrameTabInventory(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-
-        global table_values
 
         #Creando widgets
         self.lbl_description = customtkinter.CTkLabel(
@@ -28,7 +36,6 @@ class FrameTabInventory(customtkinter.CTkFrame):
         self.btn_add = customtkinter.CTkButton(self, text="Ingresar", command=self.get_data)
         self.btn_clear = customtkinter.CTkButton(self, text="Limpiar")
 
-        self.table = CTkTable(master=self, row=1, column=2, values=table_values)
 
         #Colocando los widgets
         self.lbl_description.grid(row=0, column=0, columnspan=3, sticky="ew")
@@ -42,9 +49,19 @@ class FrameTabInventory(customtkinter.CTkFrame):
         self.btn_add.grid(row=3, column=0, padx=10, pady=20)
         self.btn_clear.grid(row=3, column=1, padx=(10, 0), pady=20)
 
+        #Tabla
+        values, amout_row = self.get_data()
+        self.table = MyTable(master=self, row=amout_row, column=2, values=values)
         self.table.grid(row=4, column=0, columnspan=3, padx=20, pady=20, sticky="nesw")
 
     def get_data(self):
-        product_name = self.tb_name.get()
-        product_quantity = self.tb_quantity.get()
-        # Aquí se mostrarían los datos en un treeviewer
+        #product_name = self.tb_name.get()
+        #product_quantity = self.tb_quantity.get()
+        values = [["Nombre", "apellido"],
+                  ["Leo", "Pucheta"],
+                  ["Maxi", "Acuña"],
+                  ["Pedro", "Picapiedra"],
+                  ["Jugan", "Gomes"],
+                  ["San", "Nicolas"],
+                  ["Nicola", "Tezla"],]
+        return values, 7
