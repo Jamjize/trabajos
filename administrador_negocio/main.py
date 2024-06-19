3a2535737bb9616af9a31f6c363a0ce7887f2cdf
Leonel Pucheta <jamjizelol@gmail.com>
2024-06-19 06:54:20 -0300

    prueba de views y el nuevo widget label_frame

--- administrador_negocio/main.py
+++ administrador_negocio/main.py
@@ -2,15 +2,26 @@ import customtkinter
 from ui.components.label_entry import LabelEntryFrame
 from styles.fonts import Font, Size
 
+class ViewTest(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        label_entry = LabelEntryFrame(self)
+        label_entry2 = LabelEntryFrame(self)
+
+        label_entry.grid(row=0, column=0, pady=5)
+        label_entry2.grid(row=1, column=0, pady=5)
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.geometry("600x600")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
-        self.label_entry_1 = LabelEntryFrame(self, [["hola", (Font.DEFAULT.value, Size.DEFAULT.value), "center", 100], ["hola", (Font.DEFAULT.value, Size.DEFAULT.value), "center", 100], ["hola", (Font.DEFAULT.value, Size.DEFAULT.value), "center", 100]], orientation="vert", padx=10, pady=5)
-        self.label_entry_1.grid(row=0, column=0)
-
+        view = ViewTest(self)
+        view.grid(row=0, column=0)
 
 
 app = App()
