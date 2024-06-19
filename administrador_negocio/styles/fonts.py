a79408ca1177f2d75ce34ed3ced95b8b0d8a876a
Leonel Pucheta <jamjizelol@gmail.com>
2024-06-19 05:53:36 -0300

    agregado ayuda para ver los valor de las constantes

--- administrador_negocio/styles/fonts.py
+++ administrador_negocio/styles/fonts.py
@@ -1,10 +1,19 @@
 from enum import Enum
 
 class Font(Enum):
+    '''
+    DEFAULT = "arial"
+    BASE = "ebrima"
+    '''    
     DEFAULT = "arial"
     BASE = "ebrima"
 
 class Size(Enum):
+    '''
+        VERY_BIG = 45
+        BIG = 24
+        DEFAULT = 13
+    '''
     VERY_BIG = 45
     BIG = 24
     DEFAULT = 13
