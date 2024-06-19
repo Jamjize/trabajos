c86bc74666fd07e4e9fb881eec24ca168e1b8473
Leonel Pucheta <jamjizelol@gmail.com>
2024-06-19 06:54:00 -0300

    simplificado todo el widget para modificarse a lo básico

--- administrador_negocio/ui/components/label_entry.py
+++ administrador_negocio/ui/components/label_entry.py
@@ -1,47 +1,39 @@
 import customtkinter
 from styles.fonts import Font, Size
 
-"""
-TODO: hacer que la clase sirva para crear varias instancias, a partir de listas
-TODO: Si llegase el caso, que tengas que generar algo para que te pida si tenés
-TODO: que pedir si se crean vertical u horizontal hacelo.
-"""
 
 class LabelEntryFrame(customtkinter.CTkFrame):
-    def __init__(
-        self, 
-        master,
-        widget_parameters_list: list=[[
-            "text", 
-            (Font.DEFAULT.value, Size.DEFAULT.value), 
-            "center", 
-            100,
-        ]],
-        orientation = "horiz",
-        padx: int = 10,
-        pady: int = 10
-    ):
+    def __init__(self, 
+                master, 
+                text: str="hello", 
+                font: str=Font.DEFAULT.value, 
+                size: str=Size.DEFAULT.value,
+                text_position: str="center",
+                entry_width: int=100,
+                hint_text: str="your text"
+                ):
         super().__init__(master)
-        self.widget_count: list = []
-        self.orientation = orientation
-        self.padx = padx
-        self.pady = pady
 
-        # --Creacion widgets--
-        #* -parameters- es una lista[] con los parametros para el widget
-        for i, parameters in enumerate(widget_parameters_list):
-            self.label = customtkinter.CTkLabel(
-                master=self,
-                text=parameters[0],
-                font=parameters[1],
-                anchor=parameters[2]
-            )
-            self.entry = customtkinter.CTkEntry(master=self, width=parameters[3])
-            # Acomodando widgets dentro del frame
-            if self.orientation == "horiz":
-                self.label.grid(row=0, column=i, padx=self.padx)
-                self.entry.grid(row=1, column=i, padx=self.padx)
-            else:
-                self.label.grid(row=2*i+1, column=0, pady=(self.pady, 0))
-                self.entry.grid(row=2*i+2, column=0, pady=(0, self.pady))
-            self.widget_count.append((self.label, self.entry))
+        #--Parametros--
+        #Label
+        self.text = text
+        self.font = (font, size)
+        self.text_position = text_position
+        #Entry
+        self.entry_width = entry_width
+        self.hint_text = hint_text
+
+        #widget
+        self.label = customtkinter.CTkLabel(self,
+                                            text=self.text,
+                                            font=self.font, 
+                                            anchor=self.text_position
+                                            )
+        self.entry = customtkinter.CTkEntry(self,
+                                            width=self.entry_width,
+                                            placeholder_text=self.hint_text
+                                            )
+
+        #grid
+        self.label.grid(row=0, column=0, sticky="we")
+        self.entry.grid(row=1, column=0)
