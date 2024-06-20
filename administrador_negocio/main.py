f0fb69862c02633c1d7d3f952f2de5ff0f9e7474
Leonel Pucheta <jamjizelol@gmail.com>
2024-06-20 13:15:27 -0300

    corigiendo identación y borrando código inutil

--- administrador_negocio/main.py
+++ administrador_negocio/main.py
@@ -2,15 +2,6 @@ import customtkinter
 from ui.components.label_entry import LabelEntryFrame
 from styles.fonts import Font, Size
 
-class ViewTest(customtkinter.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-
-        label_entry = LabelEntryFrame(self)
-        label_entry2 = LabelEntryFrame(self)
-
-        label_entry.grid(row=0, column=0, pady=5)
-        label_entry2.grid(row=1, column=0, pady=5)
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -19,10 +10,6 @@ class App(customtkinter.CTk):
         self.geometry("600x600")
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-
-        view = ViewTest(self)
-        view.grid(row=0, column=0)
-
 
 app = App()
 app.mainloop()
--- administrador_negocio/ui/components/label_entry.py
+++ administrador_negocio/ui/components/label_entry.py
@@ -4,16 +4,15 @@ from styles.fonts import Font, Size
 
 class LabelEntryFrame(customtkinter.CTkFrame):
     def __init__(self, 
-                master, 
-                text: str="hello", 
-                font: str=Font.DEFAULT.value, 
-                size: str=Size.DEFAULT.value,
-                text_position: str="center",
-                entry_width: int=100,
-                hint_text: str="your text"
-                ):
+        master, 
+        text: str="hello", 
+        font: str=Font.DEFAULT.value, 
+        size: str=Size.DEFAULT.value,
+        text_position: str="center",
+        entry_width: int=100,
+        hint_text: str="your text"
+        ):
         super().__init__(master)
-
         #--Parametros--
         #Label
         self.text = text
@@ -22,18 +21,16 @@ class LabelEntryFrame(customtkinter.CTkFrame):
         #Entry
         self.entry_width = entry_width
         self.hint_text = hint_text
-
         #widget
         self.label = customtkinter.CTkLabel(self,
-                                            text=self.text,
-                                            font=self.font, 
-                                            anchor=self.text_position
-                                            )
+            text=self.text,
+            font=self.font, 
+            anchor=self.text_position
+            )
         self.entry = customtkinter.CTkEntry(self,
-                                            width=self.entry_width,
-                                            placeholder_text=self.hint_text
-                                            )
-
+            width=self.entry_width,
+            placeholder_text=self.hint_text
+            )
         #grid
         self.label.grid(row=0, column=0, sticky="we")
         self.entry.grid(row=1, column=0)
