4d1bdb10201d342036e205c8133606f829d1381f
Leonel Pucheta <jamjizelol@gmail.com>
2024-06-23 06:36:31 -0300

    Tabla con barra deslizante funciona como complemento

--- administrador_negocio/ui/components/table_bar.py
+++ administrador_negocio/ui/components/table_bar.py
@@ -3,7 +3,6 @@ from CTkTable import *
 
 
 table_values = [
-    ["Item", "Cantidad", "Unidad"],
     ["pizza", 30, "u"],
     ["Manaos cola", 20, "Ltrs"],
     ["pan", 2, "kg"],
@@ -14,35 +13,54 @@ table_values = [
     ["mayonesa", 0.2, "kg"],
     ["ketchup", 0.2, "kg"],
     ["mostaza", 0.1, "kg"],
-    ["hamburguesa", 1, "u"]
-    ]
+    ["hamburguesa", 1, "u"],
+    ["teta", 2, "3"]
+]
 
 
-class TableScrollbar(customtkinter.CTkScrollableFrame):
-    def __init__(self, master, values: list, width: int=600, height: int=600, header_color: str="red"):
-        super().__init__(master, width, height)
-        #Configs
-        self.values = values
-        self.header_color = header_color
-        #widget
-        self.table = CTkTable(self, row=12, column=3, values=self.values, header_color=self.header_color)
-        self.table.grid(row=0, column=0, padx=20, pady=20)
+class TableAndScrollbar(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, values: list):
+        super().__init__(master)
+        self.table_values = values
+        self.table = CTkTable(
+            self,
+            row=len(self.table_values),
+            column=3,
+            values=self.table_values,
+            corner_radius=1
+        )
+        self.table.grid(row=0, column=0)
+
+
+class TableFrameContainer(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        #Variables
+        self.header_values = [["Item", "Cantidad", "Unidad"]]
+        #Widgets
+        self.decorator_table = CTkTable(
+            self,
+            values=self.header_values,
+            header_color="red",
+            corner_radius=1
+        )
+        self.table_bar = TableAndScrollbar(self, values=table_values)
+        #Grid
+        self.decorator_table.grid(row=0, column=0)
+        self.table_bar.grid(row=1, column=0, sticky="we")
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        #Config
-        self.geometry("600x600")
-        self.columnconfigure((0,1), weight=1)
-        self.rowconfigure((0,1), weight=1)
-        #Widget
-        table_bar = TableScrollbar(self, values=table_values)
-        table_bar.grid(row=0, column=0)
 
-        table_bar2 = TableScrollbar(self, values=table_values)
-        table_bar2.grid(row=1, column=0)
+        self.table = TableFrameContainer(self)
+        self.table.grid(row=0, column=0)
+        self.table2 = TableFrameContainer(self)
+        self.table2.grid(row=0, column=1)
+        self.table3 = TableFrameContainer(self)
+        self.table3.grid(row=1, column=0)
+
 
 app = App()
 app.mainloop()
-
