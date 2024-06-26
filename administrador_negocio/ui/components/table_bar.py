d2bd29544c57c21f4e04c174f85fb013381ddb9a
Leonel Pucheta <jamjizelol@gmail.com>
2024-06-26 01:41:26 -0300

    componente table_bar creado, falta testear

--- administrador_negocio/ui/components/table_bar.py
+++ administrador_negocio/ui/components/table_bar.py
@@ -2,22 +2,6 @@ import customtkinter
 from CTkTable import *
 
 
-table_values = [
-    ["pizza", 30, "u"],
-    ["Manaos cola", 20, "Ltrs"],
-    ["pan", 2, "kg"],
-    ["queso", 0.5, "kg"],
-    ["tomate", 1, "kg"],
-    ["lechuga", 0.3, "kg"],
-    ["pollo", 1, "kg"],
-    ["mayonesa", 0.2, "kg"],
-    ["ketchup", 0.2, "kg"],
-    ["mostaza", 0.1, "kg"],
-    ["hamburguesa", 1, "u"],
-    ["teta", 2, "3"]
-]
-
-
 class TableAndScrollbar(customtkinter.CTkScrollableFrame):
     def __init__(self, master, values: list):
         super().__init__(master)
@@ -33,10 +17,11 @@ class TableAndScrollbar(customtkinter.CTkScrollableFrame):
 
 
 class TableFrameContainer(customtkinter.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, table_values: list):
         super().__init__(master)
         #Variables
         self.header_values = [["Item", "Cantidad", "Unidad"]]
+        self.table_values = table_values
         #Widgets
         self.decorator_table = CTkTable(
             self,
@@ -50,17 +35,3 @@ class TableFrameContainer(customtkinter.CTkFrame):
         self.table_bar.grid(row=1, column=0, sticky="we")
 
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.table = TableFrameContainer(self)
-        self.table.grid(row=0, column=0)
-        self.table2 = TableFrameContainer(self)
-        self.table2.grid(row=0, column=1)
-        self.table3 = TableFrameContainer(self)
-        self.table3.grid(row=1, column=0)
-
-
-app = App()
-app.mainloop()
